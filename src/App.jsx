867a0707daf2aae1108f9f0fe53294e959a39b3a
llammann <bd7n2v9tr.code.edu.az>
2023-11-28 05:23:41 +0400

    login register UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LoginRegister from "./components/LoginRegister";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Laman</h1> */}
+      <LoginRegister />
+    </>
+  );
+}
+
+export default App;
